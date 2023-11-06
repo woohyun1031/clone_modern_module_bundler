2b3fec81fdb6d5db875e913b1db46b7d86051f7f
KimWooHyun <ktkwhms3@gmail.com>
2023-11-07 02:23:35 +0900

    Feat: content => ast with babylon parser

--- src/clone_pack.cjs
+++ src/clone_pack.cjs
@@ -1,10 +1,14 @@
 const fs = require("fs");
+const babylon = require("babylon");
 
 let id = 0;
 
 function getAsset(path) {
   const content = fs.readFileSync(path, "utf-8");
-  console.log(content);
+  const ast = babylon.parse(content, {
+    sourceType: "module",
+  });
+  return ast;
 }
 
 function createGraph(path) {
