a8b7c7a5d90f35f778875c7b77e1921bf18a3d49
KimWooHyun <ktkwhms3@gmail.com>
2023-11-08 02:24:14 +0900

    Feat: file 내 의존성 구하는 로직 구현

--- src/clone_pack.cjs
+++ src/clone_pack.cjs
@@ -1,14 +1,31 @@
 const fs = require("fs");
 const babylon = require("babylon");
+const traverse = require("babel-traverse").default;
+const { transformFromAst } = require("babel-core");
 
-let id = 0;
+let ID = 0;
 
 function getAsset(path) {
   const content = fs.readFileSync(path, "utf-8");
   const ast = babylon.parse(content, {
     sourceType: "module",
   });
-  return ast;
+  const dependencies = [];
+  const id = ID++;
+
+  traverse(ast, {
+    enter(path) {
+      if (path.node.type === "ImportDeclaration") {
+        dependencies.push(path.node.source.value);
+      }
+    },
+  });
+
+  return {
+    id,
+    filename: path,
+    dependencies,
+  };
 }
 
 function createGraph(path) {
